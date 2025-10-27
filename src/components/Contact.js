3919a6ab77ebcc219cb74820c3f0493ec049cb02
bhanu2923 <rupa.bhanu2003@gmail.com>
2025-10-27 16:14:39 +0530

    Fix: Remove unused Link imports to resolve ESLint errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FaMapMarkerAlt, FaPhone, FaEnvelope, FaClock, FaFacebookF, FaInstagram, FaTwitter, FaPaperPlane } from 'react-icons/fa';
 import '../styles/Contact.css';
 
